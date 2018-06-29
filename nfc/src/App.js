c55fb03c783cc1423edb912e52b050723471e7f2
triplem5ds <darkkingma@hotmail.com>
2018-06-29 15:54:31 +0200

    create Profile create (ironic)

--- nfc/src/App.js
+++ nfc/src/App.js
@@ -1,22 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import UserCreate from './pages/makeProfileCreate'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-            {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          <h1 className="App-title">Add Profile</h1>
-            <h6><font color="#808080">Add a profile for a new person</font></h6>
-        </header>
-          <div className= "profileInfo">
-              <div className= "profilePicture">
-              </div>
-              <div className="username">
-              </div>
-          </div>
+      <div>
+          <UserCreate/>
       </div>
     );
   }
