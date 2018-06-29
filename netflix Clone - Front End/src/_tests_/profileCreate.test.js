c3ecd38c86d65e8354dd7b84dd7f7e9c3dddf688
triplem5ds <darkkingma@hotmail.com>
2018-06-29 17:53:13 +0200

    create profile without styling

--- netflix Clone - Front End/src/_tests_/profileCreate.test.js
+++ netflix Clone - Front End/src/_tests_/profileCreate.test.js
@@ -17,8 +17,11 @@ describe('Profile Create',()=>{
     it('should find a TextBox', () =>{
         expect(headerWrapper.find({id:'user_Name'}).length).toBe(1);
     })
-    it('should find a header', () =>{
-        expect(headerWrapper.find('.App').length).toBe(1);
+    it('should contain an image', ()=>{
+        expect(headerWrapper.find({id:'PP'}).length).toBe(1);
+    })
+    it('this image on Click should give an alret', () =>{
+        expect(headerWrapper.find({id:'urlText'}).length).toBe(1);
     })
 
 
