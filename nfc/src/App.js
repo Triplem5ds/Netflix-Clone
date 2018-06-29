60d60752282bbc8701d74091477e2fa5fe789dbe
triplem5ds <darkkingma@hotmail.com>
2018-06-29 14:45:55 +0200

    new init

--- nfc/src/App.js
+++ nfc/src/App.js
@@ -7,12 +7,16 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+            {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          <h1 className="App-title">Add Profile</h1>
+            <h6><font color="#808080">Add a profile for a new person</font></h6>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div className= "profileInfo">
+              <div className= "profilePicture">
+              </div>
+              <div className="username">
+              </div>
+          </div>
       </div>
     );
   }
